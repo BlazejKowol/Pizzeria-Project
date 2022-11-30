31e9f2d73eed4ff1ab6f0e4fc738c4866fd7102f
Błażej Kowol <kowol.blazej@gmail.com>
2022-12-01 00:55:44 +0100

    update processOrder by adding optionImage

--- src/js/script.js
+++ src/js/script.js
@@ -171,12 +171,25 @@
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = {label: 'Olives', price: 2, default: true}
           const option = param.options[optionId];
+          // create optionImage by finding the params and options
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          console.log('option image', optionImage);
+          // check if there's an option Image
+          if(optionImage) {
+            // if the option is checked, add it
+            if(formData[paramId] && formData[paramId].includes(optionId)) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+              // if the option is not checked, remove it
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
           // check if there is param with a name of paramId in formData
           if(formData[paramId] && formData[paramId].includes(optionId)) {
           // check if the option is not default
             if(!option.default == true) {
               // add option price to price variable
-              price = price + option['price'];
+              price = price + option['price'];            
             }
           } else {
             // check if the option is default
